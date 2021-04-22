3c43eb274c8e33ccd0535a284186773dce8ba027
Christian Gawron <gawron.christian@fh-swf.de>
2021-04-22 10:30:30 +0200

    Demo bot for GirlsDay 2021

---
+++ actions/actions.py
@@ -0,0 +1,34 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionAnswerAge(Action):
+    def name(self) -> Text:
+        return "action_answer_age"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        print(f"message: {tracker.latest_message}")
+        for ent in tracker.latest_message['entities']:
+            if ent['entity'] == 'PER':
+                dispatcher.utter_message(text=f"Das Alter von {ent['value']} muss ich nachsehen.")
+
+        return []
+
+class ActionAnswerCapital(Action):
+    def name(self) -> Text:
+        return "action_answer_capital"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        print(f"message: {tracker.latest_message}")
+        for ent in tracker.latest_message['entities']:
+            if ent['entity'] == 'LOC':
+                dispatcher.utter_message(text=f"Die Hauptstadt von {ent['value']} muss ich nachsehen.")
+
+        return []
